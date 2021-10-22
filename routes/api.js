f1c9a4f90cc02ac423c23dc91917c07ed2cd47b3
Andreas Noteng <andreas@noteng.no>
2021-10-22 15:10:14 +0200

    add cors, basic functionality ok

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,4 @@
+const {ObjectId} = require("mongodb");
 module.exports = function(app, db) {
     app.get('/api', function(req, res) {
         res.send('../public/index.html');
@@ -14,12 +15,37 @@ module.exports = function(app, db) {
     }
     for(let i of collections){
         app.get('/api/' + i + '/:id', function(req, res) {
-            const id = parseInt(req.params.id);
+            const id = ObjectId(req.params.id);
             console.log(req.params.id)
-            db.db('sopp').collection(i).findOne({"_id": ObjectId(id)})
+            db.db('sopp').collection(i).findOne({"_id": id})
                 .then(result => {
+                    console.log(result)
                     res.send(JSON.stringify(result))
                 })
         })
     }
+    app.post('/api/species', function(req, res){
+        const data = req.body
+        console.log(req.body)
+        db.db('sopp').collection('species').insertOne({
+            genus: data.genus,
+            species: data.species,
+            norwegian: data.norwegian,
+            english: data.english,
+            description: data.body
+        })
+            .then(result => {
+                console.log(result)
+                res.send(JSON.stringify(result))
+            })
+    })
+    app.delete('/api/species/:id', function(req, res){
+        const id = ObjectId(req.params.id);
+        // console.log(req.params.id)
+        db.db('sopp').collection('species').deleteOne({"_id": id})
+            .then(result => {
+                console.log(result)
+                res.send(JSON.stringify(result))
+            })
+    })
 }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ console.log('May Node be with you')
 const express = require('express');
 const bodyParser = require('body-parser')
 const OktaJwtVerifier = require('@okta/jwt-verifier')
-
+const cors = require('cors')
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 // const fs = require('fs');
@@ -16,12 +16,34 @@ const oktaJwtVerifier = new OktaJwtVerifier({
     clientId: process.env.OKTA_CLIENT,
     issuer: process.env.OKTA_ISSUER
 })
+app.use(cors({
+    origin: '*'
+}))
+// verify JWT token middleware
+app.use((req, res, next) => {
+    // require every request to have an authorization header
+    if (!req.headers.authorization) {
+        return next(new Error('Authorization header is required'))
+    }
+    let parts = req.headers.authorization.trim().split(' ')
+    let accessToken = parts.pop()
+    oktaJwtVerifier.verifyAccessToken(accessToken, 'api://default')
+        .then(jwt => {
+            req.user = {
+                uid: jwt.claims.uid,
+                email: jwt.claims.sub
+            }
+            next()
+        })
+        .catch(next) // jwt did not verify!
+})
 MongoClient.connect(process.env.ATLAS_URI, (err, client) => {
     if (err) return console.error(err)
     console.log('Connected to Database')
     app.listen(8080, function() {
         console.log('listening on 8080')
     })
-    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+
     require('./routes/api')(app, client)
 })
